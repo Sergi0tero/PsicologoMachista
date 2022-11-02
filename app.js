8e04c545eb08755f6e267f65f10a2af41e8f6627
User <sergioaotero26@hotmail.com>
2022-11-02 09:02:09 -0500

    desarrollo back

--- app.js
+++ app.js
@@ -29,4 +29,241 @@ var app = function(){
                 }
             }
     })
+
+    var _pj;
+
+var edr;
+
+function _pj_snippets(container) {
+  function in_es6(left, right) {
+    if (right instanceof Array || typeof right === "string") {
+      return right.indexOf(left) > -1;
+    } else {
+      if (right instanceof Map || right instanceof Set || right instanceof WeakMap || right instanceof WeakSet) {
+        return right.has(left);
+      } else {
+        return left in right;
+      }
+    }
+  }
+
+  container["in_es6"] = in_es6;
+  return container;
+}
+
+var _pj = {};
+
+
+while (cnt !== v52.length - 1) {
+  if (_pj.in_es6(v52[cnt], dat)) {
+    while (edr !== 1) {
+      if (_pj.in_es6("Hombre" && "hombre", dat)) {
+        if (val === 1) {
+          console.log("Madure");
+          edr = 1;
+        }
+
+        if (val === 2) {
+          console.log("Deje de chillar y sea hombre");
+          edr = 1;
+        }
+
+        if (val === 3) {
+          console.log("No sea loca");
+          edr = 1;
+        }
+
+        if (val === 4) {
+          console.log("Que delicado para ser hombre");
+          edr = 1;
+        }
+
+        if (val === 5) {
+          console.log("Sea hombre y listo");
+          edr = 1;
+        }
+
+        if (val === 6) {
+          console.log("No llore tanto");
+          edr = 1;
+        }
+
+        if (val === 7) {
+          console.log("Ese se su problema, no fastidie");
+          edr = 1;
+        }
+
+        if (val === 8) {
+          console.log("Guardeselo y deje de ser tan nena");
+          edr = 1;
+        }
+
+        if (val === 9) {
+          console.log("Aguantese y listo");
+          edr = 1;
+        }
+
+        if (val === 10) {
+          console.log("\u00bfY a mi que me importa?");
+          edr = 1;
+        }
+
+        if (val === 11) {
+          console.log("Pues chupe y aguante");
+          edr = 1;
+        }
+
+        if (val === 12) {
+          console.log("Guardese sus problemas y madure");
+          edr = 1;
+        }
+
+        if (val === 13) {
+          console.log("Severa flor, deje de chillar");
+          edr = 1;
+        }
+
+        if (val === 14) {
+          console.log("\u00bfY eso que me importa?");
+          edr = 1;
+        }
+
+        if (val === 15) {
+          console.log("Bueno, \u00bfy eso que o que putas me importa?");
+          edr = 1;
+        }
+
+        if (val === 16) {
+          console.log("Y si mejor se calla chilletas");
+          edr = 1;
+        }
+
+        if (val === 17) {
+          console.log("\u00bfY eso en que me incumbe?");
+          edr = 1;
+        }
+
+        if (val === 18) {
+          console.log("Solucionelo usted solo y no me joda");
+          edr = 1;
+        }
+
+        if (val === 19) {
+          console.log("Que flor mas delicada, madure");
+          edr = 1;
+        }
+
+        if (val === 20) {
+          console.log("No me importa");
+          edr = 1;
+        }
+      }
+
+      if (_pj.in_es6("Mujer" && "mujer", dat)) {
+        if (val === 1) {
+          console.log("\u00bfY si vas a terapia mejor?");
+          edr = 1;
+        }
+
+        if (val === 2) {
+          console.log("\u00bfHas buscado alguien con quien hablarlo?");
+          edr = 1;
+        }
+
+        if (val === 3) {
+          console.log("Es terrible escuchar eso");
+          edr = 1;
+        }
+
+        if (val === 4) {
+          console.log("Que terrible noticia");
+          edr = 1;
+        }
+
+        if (val === 5) {
+          console.log("Lamento escuchar eso");
+          edr = 1;
+        }
+
+        if (val === 6) {
+          console.log("Y has hablado con alguien al respecto?");
+          edr = 1;
+        }
+
+        if (val === 7) {
+          console.log("Que cosa tan fea eso");
+          edr = 1;
+        }
+
+        if (val === 8) {
+          console.log("Que vaina");
+          edr = 1;
+        }
+
+        if (val === 9) {
+          console.log("Deberias hablarlo con alguien");
+          edr = 1;
+        }
+
+        if (val === 10) {
+          console.log("Que mala noticia escuchar eso");
+          edr = 1;
+        }
+
+        if (val === 11) {
+          console.log("Que cosa tan mala escuchar eso");
+          edr = 1;
+        }
+
+        if (val === 12) {
+          console.log("Y si hablas con alguien al respecto");
+          edr = 1;
+        }
+
+        if (val === 13) {
+          console.log("Deberias comentarselo a alguien");
+          edr = 1;
+        }
+
+        if (val === 14) {
+          console.log("Se fuerte que tu puedes");
+          edr = 1;
+        }
+
+        if (val === 15) {
+          console.log("Dale que tu puedes con toda");
+          edr = 1;
+        }
+
+        if (val === 16) {
+          console.log("Con toda a darle pues");
+          edr = 1;
+        }
+
+        if (val === 17) {
+          console.log("Espero que mejores");
+          edr = 1;
+        }
+
+        if (val === 18) {
+          console.log("Mejorate pronto");
+          edr = 1;
+        }
+
+        if (val === 19) {
+          console.log("\u00bfQue cosas no?");
+          edr = 1;
+        }
+
+        if (val === 20) {
+          console.log("Terrible que estes asi");
+          edr = 1;
+        }
+      }
+    }
+  }
+
+  cnt += 1;
+}
+
 }()
